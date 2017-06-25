32ef5eba9c85e764c213084af9e3c082f404ed13
Christoph Reiter <reiter.christoph@gmail.com>
2017-06-25 17:01:43 +0200

    check-builds: Add --buildorder option which lists the PKGBUILDS in the order that they need to be build

--- msys2-check-builds.py
+++ msys2-check-builds.py
@@ -35,7 +35,28 @@ from multiprocessing import cpu_count
 from m2hlib import get_srcinfo_for_pkgbuild, package_name_is_vcs
 
 
+class cached_property(object):
+    """A read-only @property that is only evaluated once."""
+
+    def __init__(self, fget, doc=None):
+        self.fget = fget
+        self.__doc__ = doc or fget.__doc__
+        self.__name__ = name = fget.__name__
+        # these get name mangled, so caching wont work unless
+        # we mangle too
+        assert not (name.startswith("__") and not name.endswith("__")), \
+            "can't cache a dunder method"
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        obj.__dict__[self.__name__] = result = self.fget(obj)
+        return result
+
+
 class Package(object):
+
+    __packages = {}
 
     def __init__(self, pkgbuild_path, pkgname, pkgver, pkgrel):
         self.pkgbuild_path = pkgbuild_path
@@ -45,6 +66,37 @@ class Package(object):
         self.epoch = None
         self.depends = []
         self.makedepends = []
+
+        # There can be more than one package with the same name, we can't be
+        # sure which one is in the repo..
+        self.__packages.setdefault(pkgname, set()).add(self)
+
+    def __repr__(self):
+        return "<%s %s %s>" % (
+            type(self).__name__, self.pkgname, self.build_version)
+
+    @classmethod
+    def by_name(cls, pkgname):
+        return cls.__packages.get(pkgname, set())
+
+    @cached_property
+    def transitive_dependencies(self):
+        """Returns a set of transitive dependencies for this package"""
+
+        packages_todo = set([self])
+        packages_done = set()
+        deps = set()
+
+        while packages_todo:
+            p = packages_todo.pop()
+            packages_done.add(p)
+            new_deps = set(p.depends + p.makedepends)
+            deps.update(new_deps)
+            for d in new_deps:
+                for new_p in self.by_name(d):
+                    if new_p not in packages_done:
+                        packages_todo.add(new_p)
+        return deps
 
     @property
     def build_version(self):
@@ -129,6 +181,92 @@ def iter_packages(repo_path, show_progress):
     pool.join()
 
 
+def get_packages_in_repo():
+    pkgbuilds_in_repo = {}
+    text = subprocess.check_output(["pacman", "-Sl"]).decode("utf-8")
+    for line in text.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        package_name, version = line.split()[1:3]
+        pkgbuilds_in_repo[package_name] = version
+    return pkgbuilds_in_repo
+
+
+def print_build_order(packages_todo, pkgbuilds_in_repo):
+
+    pkgbuilds = {}
+    for package in packages_todo:
+        pkgbuilds.setdefault(package.pkgbuild_path, set()).add(package)
+
+    # now sort the pkgbuilds by the dependencies of the contained packages
+
+    try:
+        cmp
+    except NameError:
+        cmp = lambda a, b: (a > b) - (a < b)
+
+    def cmp_func(aa, bb):
+        """Takes two sets of packages ands sorts according to their
+        dependencies.
+        """
+
+        at = set()
+        for a in aa:
+            a_name = a.pkgname
+            at.update(a.transitive_dependencies)
+
+        bt = set()
+        for b in bb:
+            b_name = b.pkgname
+            bt.update(b.transitive_dependencies)
+
+        # make the result deterministic, packages with fewer dependencies first
+        a_key = (len(at), a_name)
+        b_key = (len(bt), b_name)
+
+        if a_name in bt and b_name in at:
+            # cyclic!
+            return cmp(a_key, b_key)
+        elif a_name in bt:
+            return -1
+        elif b_name in at:
+            return 1
+        else:
+            return cmp(a_key, b_key)
+
+    real_cmp = lambda a, b: cmp_func(a[1], b[1])
+
+    if sys.version_info[0] == 3:
+        from functools import cmp_to_key
+
+        key = cmp_to_key(real_cmp)
+
+        def do_sort(x):
+            return sorted(x, key=key)
+    else:
+        def do_sort(x):
+            return sorted(x, cmp=real_cmp)
+
+    order = [i[0] for i in do_sort(pkgbuilds.items())]
+
+    for path in order:
+        print(path)
+
+
+def print_table(packages_todo, pkgbuilds_in_repo):
+    for package in sorted(packages_todo, key=lambda p: p.pkgname):
+        if package.pkgname not in pkgbuilds_in_repo:
+            print("%-50s local=%-25s repo=%-25s %s" % (
+                package.pkgname, package.build_version, "missing",
+                package.pkgbuild_path))
+        else:
+            repo_version = pkgbuilds_in_repo[package.pkgname]
+            print("%-50s local=%-25s repo=%-25s %s" % (
+                package.pkgname, package.build_version, repo_version,
+                package.pkgbuild_path))
+
+
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -140,38 +278,31 @@ def main(argv):
                         help="show progress of parsing PKGBUILD files")
     parser.add_argument('--show-vcs', action='store_true',
                         help="show VCS packages")
+    parser.add_argument('--buildorder', action='store_true',
+                        help="List PKGFILES which need to be build in the "
+                             "order they need to be build")
     args = parser.parse_args(argv[1:])
 
-    pkgbuilds_in_repo = {}
-    text = subprocess.check_output(["pacman", "-Sl"]).decode("utf-8")
-    for line in text.splitlines():
-        line = line.strip()
-        if not line:
-            continue
-        package_name, version = line.split()[1:3]
-        pkgbuilds_in_repo[package_name] = version
-
     repo_path = os.path.abspath(args.path)
-    show_progress = args.show_progress
 
-    packages = []
-    for package in iter_packages(repo_path, show_progress):
-        packages.append(package)
+    pkgbuilds_in_repo = get_packages_in_repo()
 
-    for package in sorted(packages, key=lambda p: p.pkgname):
+    packages_todo = set()
+    for package in iter_packages(repo_path, args.show_progress):
         if not args.show_vcs and package_name_is_vcs(package.pkgname):
             continue
         if package.pkgname not in pkgbuilds_in_repo:
             if args.show_missing:
-                print("%-50s local=%-25s repo=%-25s %s" % (
-                    package.pkgname, package.build_version, "missing",
-                    package.pkgbuild_path))
+                packages_todo.add(package)
         else:
             repo_version = pkgbuilds_in_repo[package.pkgname]
             if package.build_version != repo_version:
-                print("%-50s local=%-25s repo=%-25s %s" % (
-                    package.pkgname, package.build_version, repo_version,
-                    package.pkgbuild_path))
+                packages_todo.add(package)
+
+    if args.buildorder:
+        print_build_order(packages_todo, pkgbuilds_in_repo)
+    else:
+        print_table(packages_todo, pkgbuilds_in_repo)
 
 
 if __name__ == "__main__":
