cd79b29b60171d9def3733a93e80db1b497da827
Christoph Reiter <reiter.christoph@gmail.com>
2017-06-27 20:42:45 +0200

    Add some basic tests

--- m2hlib/utils.py
+++ m2hlib/utils.py
@@ -59,22 +59,3 @@ def progress(total):
     update(0)
     yield update
     update(0, True)
-
-
-class cached_property(object):
-    """A read-only @property that is only evaluated once."""
-
-    def __init__(self, fget, doc=None):
-        self.fget = fget
-        self.__doc__ = doc or fget.__doc__
-        self.__name__ = name = fget.__name__
-        # these get name mangled, so caching wont work unless
-        # we mangle too
-        assert not (name.startswith("__") and not name.endswith("__")), \
-            "can't cache a dunder method"
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        obj.__dict__[self.__name__] = result = self.fget(obj)
-        return result
---
+++ tests/test_main.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Christoph Reiter
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+
+from m2hlib import utils, pacman, srcinfo
+
+
+def test_utils():
+    assert not utils.package_name_is_vcs("foo")
+    assert utils.package_name_is_vcs("foo-git")
+
+    with utils.progress(1) as update:
+        update(1)
+
+
+def test_pacman():
+    pacman.PacmanPackage.get_all_packages()
+    pacman.PacmanPackage.get_installed_packages()
+
+
+def test_srcinfo():
+    packages = srcinfo.SrcInfoPackage.for_srcinfo("foo", """\
+pkgbase = mingw-w64-gtk3
+        pkgdesc = GObject-based multi-platform GUI toolkit (v3) (mingw-w64)
+        pkgver = 3.22.16
+        pkgrel = 1
+        url = http://www.gtk.org
+        install = gtk3-x86_64.install
+        arch = any
+        license = LGPL
+        makedepends = mingw-w64-x86_64-gcc
+        makedepends = mingw-w64-x86_64-pkg-config
+        makedepends = mingw-w64-x86_64-python2
+        makedepends = mingw-w64-x86_64-gobject-introspection
+        makedepends = autoconf
+        makedepends = automake
+        makedepends = libtool
+        depends = mingw-w64-x86_64-gcc-libs
+        depends = mingw-w64-x86_64-adwaita-icon-theme
+        depends = mingw-w64-x86_64-atk
+        depends = mingw-w64-x86_64-cairo
+        depends = mingw-w64-x86_64-gdk-pixbuf2
+        depends = mingw-w64-x86_64-glib2
+        depends = mingw-w64-x86_64-json-glib
+        depends = mingw-w64-x86_64-libepoxy
+        depends = mingw-w64-x86_64-pango
+        depends = mingw-w64-x86_64-shared-mime-info
+        options = strip
+        options = !debug
+        options = staticlibs
+        source = https://download.gnome.org/sources/gtk+/3.22/gtk+-3.22.16.tar.xz
+        sha256sums = 3e0c3ad01f3c8c5c9b1cc1ae00852bd55164c8e5a9c1f90ba5e07f14f175fe2c
+
+pkgname = mingw-w64-x86_64-gtk3
+
+pkgbase = mingw-w64-gtk3
+        pkgdesc = GObject-based multi-platform GUI toolkit (v3) (mingw-w64)
+        pkgver = 3.22.16
+        pkgrel = 1
+        url = http://www.gtk.org
+        install = gtk3-i686.install
+        arch = any
+        license = LGPL
+        makedepends = mingw-w64-i686-gcc
+        makedepends = mingw-w64-i686-pkg-config
+        makedepends = mingw-w64-i686-python2
+        makedepends = mingw-w64-i686-gobject-introspection
+        makedepends = autoconf
+        makedepends = automake
+        makedepends = libtool
+        depends = mingw-w64-i686-gcc-libs
+        depends = mingw-w64-i686-adwaita-icon-theme
+        depends = mingw-w64-i686-atk
+        depends = mingw-w64-i686-cairo
+        depends = mingw-w64-i686-gdk-pixbuf2
+        depends = mingw-w64-i686-glib2
+        depends = mingw-w64-i686-json-glib
+        depends = mingw-w64-i686-libepoxy
+        depends = mingw-w64-i686-pango
+        depends = mingw-w64-i686-shared-mime-info
+        options = strip
+        options = !debug
+        options = staticlibs
+        source = https://download.gnome.org/sources/gtk+/3.22/gtk+-3.22.16.tar.xz
+        sha256sums = 3e0c3ad01f3c8c5c9b1cc1ae00852bd55164c8e5a9c1f90ba5e07f14f175fe2c
+
+pkgname = mingw-w64-i686-gtk3
+""")
+
+    assert len(packages) == 2
+    packages = sorted(packages, key=lambda x: x.pkgname)
+
+    assert packages[0].pkgname == "mingw-w64-i686-gtk3"
+    assert packages[1].pkgname == "mingw-w64-x86_64-gtk3"
+    assert packages[0].pkgbase == "mingw-w64-gtk3"
+    assert "mingw-w64-i686-glib2" in packages[0].depends
+    assert "mingw-w64-i686-pkg-config" in packages[0].makedepends
+    assert packages[0].build_version == "3.22.16-1"
